616ff6dc5e66c233446d42091d1585b73df6d76a
PeeHaa <info@pieterhordijk.com>
2013-08-18 21:49:00 +0200

    Added environment files

---
+++ init.development.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Setup error reporting
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+
+/**
+ * Setup timezone
+ */
+ini_set('date.timezone', 'Europe/Amsterdam');
