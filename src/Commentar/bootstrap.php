07c07d7e4a96140d8c3b29425da2ebe90de8e8a7
PeeHaa <info@pieterhordijk.com>
2013-08-18 21:17:53 +0200

    Added library bootstrap

---
+++ src/Commentar/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This bootstraps the Commentar library
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    0.0.1
+ */
+namespace Commentar;
+
+use Commentar\Core\Autoloader;
+
+require_once __DIR__ . '/Core/Autoloader.php';
+
+$autoloader = new Autoloader(__NAMESPACE__, dirname(__DIR__));
+$autoloader->register();
