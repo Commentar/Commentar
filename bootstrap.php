4d9e6b4fbd986f1c0c2595a5df2ccb2bf265e78b
PeeHaa <info@pieterhordijk.com>
2013-08-25 03:32:11 +0200

    Implemented resource loader

--- bootstrap.php
+++ bootstrap.php
@@ -13,7 +13,10 @@
 namespace Commentar;
 
 use Commentar\Core\Autoloader,
-    Commentar\Presentation\Theme;
+    Commentar\Storage\ArrayStorage,
+    Commentar\Http\Request,
+    Commentar\Presentation\Theme,
+    Commentar\Presentation\ResourceLoader;
 
 /**
  * Bootstrap the Commentar library
@@ -38,11 +41,32 @@ if(php_sapi_name() === 'cli') {
     return;
 }
 
-if ($_SERVER['REQUEST_URI'] == '/style/style.css') {
-    header('Content-Type: text/css');
-    require_once __DIR__ . '/themes/commentar/style/style.css';
+/**
+ * Setup the request object
+ */
+$request = new Request(
+    new ArrayStorage($_GET),
+    new ArrayStorage($_POST),
+    new ArrayStorage($_SERVER),
+    new ArrayStorage($_FILES)
+);
+
+/**
+ * Setup the resource loader
+ */
+$resourceLoader = new ResourceLoader();
+
+/**
+ * Setup the theme loader
+ */
+$theme = new Theme(__DIR__ . '/themes/', $resourceLoader);
+
+/**
+ * Load external resources (stylesheet, images etc)
+ */
+if ($request->isResource()) {
+    echo $theme->loadResource($request->getPath());
     exit;
 }
 
-$theme = new Theme(['commentar'], __DIR__ . '/themes/');
 echo $theme->load();
