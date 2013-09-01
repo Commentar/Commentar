5c731257ac7fc5129036ac78bf983a98c427740e
PeeHaa <info@pieterhordijk.com>
2013-09-02 00:34:15 +0200

    Added no matching route exception

---
+++ src/Commentar/Router/NoMatchingRouteException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when no route matches the request
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Commentar\Router;
+
+/**
+ * Exception which gets thrown when no route matches the request
+ *
+ * @category   Commentar
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class NoMatchingRouteException extends \Exception
+{
+}
