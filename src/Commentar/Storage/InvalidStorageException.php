cc126ed3aedc5cf764b5f702251778339319200b
PeeHaa <info@pieterhordijk.com>
2013-09-08 23:40:28 +0200

    Added InvalidStorageException

---
+++ src/Commentar/Storage/InvalidStorageException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when trying to create an invalid storage
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Commentar\Storage;
+
+/**
+ * Exception which gets thrown when trying to create an invalid storage
+ *
+ * @category   Commentar
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidStorageException extends \Exception
+{
+}
