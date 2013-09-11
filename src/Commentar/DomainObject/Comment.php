ae151ac5472784bfff52279aaf3f798376a15364
PeeHaa <info@pieterhordijk.com>
2013-09-11 21:35:10 +0200

    Added comment domain object

---
+++ src/Commentar/DomainObject/Comment.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Comment domain object
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @package    DomainObject
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Commentar\DomainObject;
+
+/**
+ * Comment domain object
+ *
+ * @category   Commentar
+ * @package    DomainObject
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Comment
+{
+    /**
+     * @var int The id of the user
+     */
+    protected $id;
+
+    /**
+     * @var \Commentar\DomainObject\User The user who posted the comment
+     */
+    protected $user;
+
+    /**
+     * @var null|int The id of the parent
+     */
+    protected $parent;
+
+    /**
+     * @var string The content of the comment
+     */
+    protected $content;
+
+    /**
+     * @var \DateTime The timestamp
+     */
+    protected $timestamp;
+
+    /**
+     * @var boolean Whether the comment has been reviewed
+     */
+    protected $isReviewed = false;
+
+    /**
+     * @var boolean Whether the comment has been moderated
+     */
+    protected $isModerated = false;
+
+    /**
+     * Fills the object with data
+     *
+     * @param array $data The data used to fill the object
+     *
+     * @throws \Commentar\DomainObject\InvalidPropertyException When trying to fill an undefined property
+     */
+    public function fill(array $data)
+    {
+        foreach ($data as $name => $value) {
+            if (!property_exists($this, $name)) {
+                throw new InvalidPropertyException('Trying to fill an invalid property (`' . $name . '`).');
+            }
+
+            $setter = 'set' . ucfirst(strtolower($name));
+            $this->$setter($value);
+        }
+    }
+
+    /**
+     * Sets the id of the comment
+     *
+     * @param int $id The id of the comment
+     */
+    protected function setId($id)
+    {
+        $this->id = (int) $id;
+    }
+
+    /**
+     * Gets the id of the comment
+     *
+     * @return int The id of the comment
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets the user
+     *
+     * @param \Commentar\DomainObject\User The user
+     */
+    protected function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Gets the user
+     *
+     * @return \Commentar\DomainObject\User The user
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Sets the id of the parent comment if any
+     *
+     * @param int $parent The optional id of the parent
+     */
+    protected function setParent($parent)
+    {
+        $this->parent = (int) $parent;
+    }
+
+    /**
+     * Gets the id of the parent comment
+     *
+     * @return null|int The parent
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Sets the content of the comment
+     *
+     * @param string $content The content of the comment
+     */
+    protected function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Gets the content of the comment
+     *
+     * @return string The content of the comment
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Sets the timestamp of the comment
+     *
+     * @param \DateTime $timestamp The timestamp of the comment
+     */
+    protected function setTimestamp(\DateTime $timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * Gets the timestamp of the comment
+     *
+     * @return \DateTime The timestamp of the comment
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * Sets the reviewed status of the comment
+     *
+     * @param boolean $isReviewed Whether the comment is reviewed
+     */
+    protected function setIsReviewed($isReviewed)
+    {
+        $this->isReviewed = (bool) $isReviewed;
+    }
+
+    /**
+     * Gets the reviewed status of the comment
+     *
+     * @return boolean Whether the comment is reviewed
+     */
+    public function isReviewed()
+    {
+        return $this->isReviewed;
+    }
+
+    /**
+     * Sets the moderation status of the comment
+     *
+     * @param boolean $isModerated Whether the comment is moderated
+     */
+    protected function setIsModerated($isModerated)
+    {
+        $this->isModerated = (bool) $isModerated;
+    }
+
+    /**
+     * Gets the moderation status of the comment
+     *
+     * @return boolean Whether the comment is moderated
+     */
+    public function isModerated()
+    {
+        return $this->isModerated;
+    }
+}
---
+++ test/Unit/DomainObject/CommentTest.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace CommentarTest\DomainObject;
+
+use Commentar\DomainObject\Comment;
+
+class CommentTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testConstructCorrectInstance()
+    {
+        $comment = new Comment();
+
+        $this->assertInstanceOf('\\Commentar\\DomainObject\\Comment', $comment);
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     */
+    public function testFillInvalidProperty()
+    {
+        $comment = new Comment();
+
+        $this->setExpectedException('\\Commentar\\DomainObject\\InvalidPropertyException');
+
+        $comment->fill(['undefinedProperty' => true]);
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setId
+     * @covers Commentar\DomainObject\Comment::getId
+     */
+    public function testGetIdFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['id' => 1]);
+
+        $this->assertSame(1, $comment->getId());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setId
+     * @covers Commentar\DomainObject\Comment::getId
+     */
+    public function testGetIdCasted()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['id' => '1']);
+
+        $this->assertSame(1, $comment->getId());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::getId
+     */
+    public function testGetIdDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertNull($comment->getId());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setUser
+     * @covers Commentar\DomainObject\Comment::getUser
+     */
+    public function testGetUserFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['user' => $this->getMock('\\Commentar\\DomainObject\\User')]);
+
+        $this->assertInstanceOf('\\Commentar\\DomainObject\\User', $comment->getUser());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::getUser
+     */
+    public function testGetUserDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertNull($comment->getUser());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setParent
+     * @covers Commentar\DomainObject\Comment::getParent
+     */
+    public function testGetParentFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['parent' => 1]);
+
+        $this->assertSame(1, $comment->getParent());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setParent
+     * @covers Commentar\DomainObject\Comment::getParent
+     */
+    public function testGetParentCasted()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['parent' => '1']);
+
+        $this->assertSame(1, $comment->getParent());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::getParent
+     */
+    public function testGetParentDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertNull($comment->getParent());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setContent
+     * @covers Commentar\DomainObject\Comment::getContent
+     */
+    public function testGetContentFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['content' => 'foo']);
+
+        $this->assertSame('foo', $comment->getContent());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::getContent
+     */
+    public function testGetContentDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertNull($comment->getContent());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setTimestamp
+     * @covers Commentar\DomainObject\Comment::getTimestamp
+     */
+    public function testGetTimestampFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['timestamp' => new \DateTime('2013-01-01 00:00:00')]);
+
+        $timestamp = $comment->getTimestamp();
+        $this->assertInstanceOf('\\DateTime', $timestamp);
+        $this->assertSame('2013-01-01 00:00:00', $timestamp->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::getTimestamp
+     */
+    public function testGetTimestampDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertNull($comment->getTimestamp());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setIsReviewed
+     * @covers Commentar\DomainObject\Comment::isReviewed
+     */
+    public function testIsReviewedFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['isReviewed' => true]);
+
+        $this->assertSame(true, $comment->isReviewed());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setIsReviewed
+     * @covers Commentar\DomainObject\Comment::isReviewed
+     */
+    public function testIsReviewedCasted()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['isReviewed' => 1]);
+
+        $this->assertSame(true, $comment->isReviewed());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::isReviewed
+     */
+    public function testIsReviewedDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertFalse($comment->isReviewed());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setIsModerated
+     * @covers Commentar\DomainObject\Comment::isModerated
+     */
+    public function testIsModeratedFilled()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['isModerated' => true]);
+
+        $this->assertSame(true, $comment->isModerated());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::fill
+     * @covers Commentar\DomainObject\Comment::setIsModerated
+     * @covers Commentar\DomainObject\Comment::isModerated
+     */
+    public function testIsModeratedCasted()
+    {
+        $comment = new Comment();
+
+        $comment->fill(['isModerated' => 1]);
+
+        $this->assertSame(true, $comment->isModerated());
+    }
+
+    /**
+     * @covers Commentar\DomainObject\Comment::isModerated
+     */
+    public function testIsModeratedDefault()
+    {
+        $comment = new Comment();
+
+        $this->assertFalse($comment->isModerated());
+    }
+}
