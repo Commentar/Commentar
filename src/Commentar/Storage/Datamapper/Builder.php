839c5df037e9895e99a6f8a34081635188140e2a
PeeHaa <info@pieterhordijk.com>
2013-09-14 16:29:36 +0200

    Added datamapper factory interface

---
+++ src/Commentar/Storage/Datamapper/Builder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Interface for storage datamapper factories
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @package    Storage
+ * @subpackage Datamapper
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Commentar\Storage\Json;
+
+/**
+ * Interface for storage datamapper factories
+ *
+ * @category   Commentar
+ * @package    Storage
+ * @subpackage Datamapper
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Builder
+{
+    /**
+     * Builds the requested datamapper
+     *
+     * @param string $name The name of the data mapper to build
+     *
+     * @return object The datamapper
+     */
+    public function build($name);
+}
