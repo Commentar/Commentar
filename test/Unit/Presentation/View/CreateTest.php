0759bdb78286e094c7b1a8d0d24d686f5a2936e4
PeeHaa <info@pieterhordijk.com>
2013-09-23 21:16:41 +0200

    Implemented thread creation

---
+++ test/Unit/Presentation/View/CreateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CommentarTest\Presentation\View;
+
+use Commentar\Presentation\View\Create;
+
+class CreateTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Commentar\Presentation\View\Create::__construct
+     */
+    public function testConstructCorrectAbstractInstance()
+    {
+        $view = new Create(
+            $this->getMock('\\Commentar\\Presentation\\ThemeLoader'),
+            $this->getMock('\\Commentar\\ServiceBuilder\\Builder')
+        );
+
+        $this->assertInstanceOf('\\Commentar\\Presentation\\View\\View', $view);
+    }
+
+    /**
+     * @covers Commentar\Presentation\View\Create::__construct
+     */
+    public function testConstructCorrectInstance()
+    {
+        $view = new Create(
+            $this->getMock('\\Commentar\\Presentation\\ThemeLoader'),
+            $this->getMock('\\Commentar\\ServiceBuilder\\Builder')
+        );
+
+        $this->assertInstanceOf('\\Commentar\\Presentation\\View\\Create', $view);
+    }
+
+    /**
+     * @covers Commentar\Presentation\View\Create::__construct
+     * @covers Commentar\Presentation\View\Create::renderTemplate
+     */
+    public function testRenderTemplate()
+    {
+        $theme = $this->getMock('\\Commentar\\Presentation\\ThemeLoader');
+        $theme->expects($this->any())
+            ->method('getFile')
+            ->will($this->returnValue(__DIR__ . '/../../../Mocks/themes/bar/page.phtml'));
+
+        $view = new Create($theme, $this->getMock('\\Commentar\\ServiceBuilder\\Builder'));
+
+        $this->assertSame('bartheme', $view->renderTemplate());
+    }
+}
