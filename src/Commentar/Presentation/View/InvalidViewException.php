91c93614abfbaf52fd968009b91831545585a5b4
PeeHaa <info@pieterhordijk.com>
2013-09-29 21:58:36 +0200

    Added invalid view exception

---
+++ src/Commentar/Presentation/View/InvalidViewException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when trying to build an invalid view
+ *
+ * PHP version 5.4
+ *
+ * @category   Commentar
+ * @package    Presentation
+ * @subpackage View
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Commentar\Presentation\View;
+
+/**
+ * Exception which gets thrown when trying to build an invalid view
+ *
+ * @category   Commentar
+ * @package    Presentation
+ * @subpackage View
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidViewException extends \Exception
+{
+}
