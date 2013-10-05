a2e6c5e63bd3c16ec7634d50854ad79f4a739bb8
PeeHaa <info@pieterhordijk.com>
2013-10-05 23:29:37 +0200

    Added delete result view

--- test/Unit/Presentation/View/DeleteResultTest.php
+++ test/Unit/Presentation/View/DeleteResultTest.php
@@ -2,16 +2,16 @@
 
 namespace CommentarTest\Presentation\View;
 
-use Commentar\Presentation\View\ConfirmDelete;
+use Commentar\Presentation\View\DeleteResult;
 
-class ConfirmDeleteTest extends \PHPUnit_Framework_TestCase
+class DeleteResultTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @covers Commentar\Presentation\View\ConfirmDelete::__construct
+     * @covers Commentar\Presentation\View\DeleteResult::__construct
      */
     public function testConstructCorrectAbstractInstance()
     {
-        $view = new ConfirmDelete(
+        $view = new DeleteResult(
             $this->getMock('\\Commentar\\Presentation\\ThemeLoader'),
             $this->getMock('\\Commentar\\ServiceBuilder\\Builder')
         );
@@ -20,21 +20,21 @@ class ConfirmDeleteTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Commentar\Presentation\View\ConfirmDelete::__construct
+     * @covers Commentar\Presentation\View\DeleteResult::__construct
      */
     public function testConstructCorrectInstance()
     {
-        $view = new ConfirmDelete(
+        $view = new DeleteResult(
             $this->getMock('\\Commentar\\Presentation\\ThemeLoader'),
             $this->getMock('\\Commentar\\ServiceBuilder\\Builder')
         );
 
-        $this->assertInstanceOf('\\Commentar\\Presentation\\View\\ConfirmDelete', $view);
+        $this->assertInstanceOf('\\Commentar\\Presentation\\View\\DeleteResult', $view);
     }
 
     /**
-     * @covers Commentar\Presentation\View\ConfirmDelete::__construct
-     * @covers Commentar\Presentation\View\ConfirmDelete::renderTemplate
+     * @covers Commentar\Presentation\View\DeleteResult::__construct
+     * @covers Commentar\Presentation\View\DeleteResult::renderTemplate
      */
     public function testRenderTemplate()
     {
@@ -43,7 +43,7 @@ class ConfirmDeleteTest extends \PHPUnit_Framework_TestCase
             ->method('getFile')
             ->will($this->returnValue(__DIR__ . '/../../../Mocks/themes/bar/page.phtml'));
 
-        $view = new ConfirmDelete($theme, $this->getMock('\\Commentar\\ServiceBuilder\\Builder'));
+        $view = new DeleteResult($theme, $this->getMock('\\Commentar\\ServiceBuilder\\Builder'));
 
         $this->assertSame('bartheme', $view->renderTemplate());
     }
